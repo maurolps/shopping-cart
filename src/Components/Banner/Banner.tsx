f6cac497e57f0a1c726a22556819aa65edd83de1
maurolps <boroengenharia@gmail.com>
2024-02-21 22:07:31 -0300

    Update ThreeJS elements

--- src/Components/Banner/Banner.tsx
+++ src/Components/Banner/Banner.tsx
@@ -1,33 +1,49 @@
 import { Canvas } from "@react-three/fiber";
 import { Shoe3d } from "../Shoe3d";
-import { Environment, OrbitControls } from "@react-three/drei";
+import { OrbitControls } from "@react-three/drei";
 
 export default function Banner() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-4  p-10 bg-foreground ">
+      <div className="grid grid-cols-2 gap-4  p-10 bg-slate-500 relative">
         <div className=" flex flex-col gap-6 max-w-lg ">
-          <span className="font-revans text-7xl text-start">
+          <span className="font-revans text-7xl text-end z-10 text-white pointer-events-none select-none pr-6">
             Fitness, <br /> Meets Comfort.
           </span>
-          <hr />
-          <span className="text-start text-sm max-w-sm text-text-variant">
-            Our mission is to elevate your every step, offering an extensive
-            selection of footwear that merges fitness-forward trends with
-            unparalleled comfort and quality.
-          </span>
-          <button className="bg-call text-white font-bold w-fit p-2">
-            SHOP NOW
-          </button>
         </div>
-        <div className="max-w-sm">
-          <Canvas dpr={2}>
-            <Environment preset="studio" />
-            <directionalLight position={[1, 10, 2]} />
+        <div className="flex flex-col gap-6 relative">
+          <div className="z-10 flex justify-center items-end h-full pointer-events-none select-none min-h-64">
+            <span className="text-start text-sm max-w-sm text-text-variant ">
+              Our mission is to elevate your every step, offering an extensive
+              selection of footwear that merges fitness-forward trends with
+              unparalleled comfort and quality.{" "}
+              <button className="bg-call text-white text-[10px] font-bold w-fit px-1 pointer-events-auto">
+                TAKE A LOOK
+              </button>
+            </span>
+          </div>
+          <div className="w-full h-full absolute aspect-square">
+            <Canvas dpr={2} camera={{ position: [10, 5, 30], fov: 10 }}>
+              <directionalLight
+                position={[1, 10, 2]}
+                color={"#00ccff"}
+                intensity={1.5}
+              />
 
-            <Shoe3d />
-            <OrbitControls />
-          </Canvas>
+              <Shoe3d />
+
+              <OrbitControls
+                enableZoom={false}
+                enablePan={false}
+                autoRotate={true}
+                autoRotateSpeed={0.5}
+                maxPolarAngle={Math.PI / 2.2}
+                minPolarAngle={0}
+                maxAzimuthAngle={Math.PI / 1}
+                minAzimuthAngle={-Math.PI / 1}
+              />
+            </Canvas>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Shoe3d.tsx
+++ src/Components/Shoe3d.tsx
@@ -1,30 +1,34 @@
-import { useGLTF } from "@react-three/drei";
+import { Grid, useGLTF } from "@react-three/drei";
 
 export function Shoe3d() {
   const { nodes } = useGLTF("/nike.glb");
+  const base = nodes["Shoe-Base"];
+  const shoe = nodes["Shoe-Shoe"];
+  // base.material.side = 1;
+  console.log(shoe.material);
 
   return (
     <>
-      {console.log(nodes)}
-
-      <mesh
-        geometry={nodes["Shoe-Shoe"].geometry}
-        position={[0, 0, 0]}
-        scale={[5, 5, 5]}
-      >
-        <meshPhongMaterial
-          color={"#f2f2f2"}
-          specular={"#71ff3d"}
-          shininess={30}
+      <group dispose={null} rotation={[-0.08, 0, 0]}>
+        <group scale={[0.08, 0.08, 0.08]} position={[0, 1, 0]}>
+          <mesh geometry={base.geometry} material={base.material} />
+          <mesh geometry={shoe.geometry} material={shoe.material} />
+        </group>
+        <Grid
+          args={[0.2, 0.2]}
+          position={[0, -0.6, 0]}
+          rotation={[0, 0, 0]}
+          cellSize={0.8}
+          cellThickness={1}
+          cellColor={"#8b8b8b"}
+          sectionSize={2}
+          sectionThickness={1}
+          sectionColor={"#ff6666"}
+          fadeDistance={38}
+          fadeStrength={0.2}
+          infiniteGrid={true}
         />
-      </mesh>
-      <mesh
-        geometry={nodes["Shoe-Base"].geometry}
-        position={[0, 0, 0]}
-        scale={[5, 5, 5]}
-      >
-        <meshPhongMaterial color={"black"} specular={"black"} shininess={30} />
-      </mesh>
+      </group>
     </>
   );
 }
