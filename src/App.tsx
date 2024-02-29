06cbe3a3fa546104a09d96f7c7eed313457d4cea
maurolps <boroengenharia@gmail.com>
2024-02-28 21:14:22 -0300

    update ProductCard component

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,28 @@ import { Header } from "./Components/Header/Header";
 import ProductCard from "./Components/ProductCard";
 
 export default function App() {
+  const mockData = [
+    {
+      id: 1,
+      name: "Nike Air Max 320 EVO Inspire",
+      price: 210,
+      sale: 0.35,
+      stars: 4.5,
+    },
+    { id: 2, name: "Adidas Ultraboost", price: 139.22, sale: 0, stars: 4 },
+  ] as const;
   return (
     <>
       <header className="bg-background shadow-sm">
         <Header />
       </header>
       <main className="flex justify-center w-full px-4 flex-col gap-5">
-        <section>{/* <Banner /> */}</section>
         <section>
-          <ProductCard />
+          <Banner />
+        </section>
+        <section className="flex flex-row gap-3">
+          <ProductCard product={mockData[0]} />
+          <ProductCard product={mockData[1]} />
         </section>
       </main>
     </>
--- src/Components/ProductCard.tsx
+++ src/Components/ProductCard.tsx
@@ -1,29 +1,46 @@
 import Rating from "@mui/material/Rating";
 
-export default function ProductCard() {
+type ProductCardProps = {
+  product: {
+    id: number;
+    name: string;
+    price: number;
+    sale: number;
+    stars: number;
+  };
+};
+
+export default function ProductCard({ product }: ProductCardProps) {
+  const salePrice = () => (product.price * (1 - product.sale)).toFixed(2);
   return (
     <>
       <div className="  border-2 border-foreground p-3 w-fit flex flex-col">
         <div className="bg-foreground w-36 h-36"></div>
-        <div className="text-text text-sm text-start font-bold max-w-36">
-          Nike Air Max 320 EVO Inspire
+        <div className="text-text text-sm text-start font-bold max-w-36 min-h-10">
+          {product.name}
         </div>
         <div>
           <Rating
             name="Shoe Stars"
-            value={4.5}
+            value={product.stars}
             precision={0.5}
             readOnly
             size="small"
           />
         </div>
         <div className="text-primary text-md text-start font-extrabold max-w-36">
-          $159,23
+          ${product.sale === 0 ? product.price : salePrice()}
         </div>
-        <div className="flex gap-2 text-sm">
-          <span className="text-text-variant line-through">299,44</span>
-          <span>35% off</span>
-        </div>
+        {product.sale !== 0 && (
+          <div className="flex gap-2 text-sm">
+            <span className="text-text-variant line-through">
+              ${product.price}
+            </span>
+            <span className="text-call text-xs bg-red-50 pl-1 pr-1">
+              {product.sale * 100}% off
+            </span>
+          </div>
+        )}
       </div>
     </>
   );
