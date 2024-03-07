16780fea97e2957a3db94d2503e5a1e86c8bfe9d
maurolps <boroengenharia@gmail.com>
2024-03-06 21:21:45 -0300

    update Checkout UI

--- src/Components/Checkout/Checkout.tsx
+++ src/Components/Checkout/Checkout.tsx
@@ -2,6 +2,7 @@ import Stepper from "@mui/material/Stepper";
 import Step from "@mui/material/Step";
 import StepLabel from "@mui/material/StepLabel";
 import { useState } from "react";
+import { StepContent } from "@mui/material";
 export default function Checkout() {
   const steps = ["Shipping address", "Payment details", "Review"];
   const stepDescription = ["Shipping", "Payment", "Review"];
@@ -17,33 +18,56 @@ export default function Checkout() {
 
   return (
     <>
-      <span className=" text-3xl font-bold">Checkout</span>
-      <Stepper activeStep={activeStep} alternativeLabel>
-        {steps.map((step) => (
-          <Step key={step}>
-            <StepLabel>{step}</StepLabel>
-          </Step>
-        ))}
-      </Stepper>
+      <div className="flex gap-2 flex-col">
+        <div className="w-[500px] p-3 shadow">
+          <div className="flex flex-col gap-2">
+            <span className="text-lg text-text font-semibold">Checkout</span>
+            <hr />
+            <div className="div">
+              <Stepper activeStep={activeStep} orientation="vertical">
+                {steps.map((step, index) => (
+                  <Step key={step}>
+                    <StepLabel
+                      icon={
+                        <span className="text-white text-xs font-bold bg-primary w-5 h-5 rounded-full flex justify-center items-center">
+                          {index + 1}
+                        </span>
+                      }
+                    >
+                      <span className="text-text">{step}</span>
+                    </StepLabel>
+                    <StepContent>
+                      <div className="min-h-60 bg-foreground p-2 mb-4">
+                        <span className="text-text-variant">
+                          {stepDescription[index]}
+                        </span>
+                      </div>
 
-      <p className="text-center text-lg font-bold">
-        {stepDescription[activeStep]}
-      </p>
-      <div className="flex gap-2 m-auto">
-        <button
-          className="bg-primary text-white text-sm px-4 p-2 w-fit disabled:opacity-50"
-          onClick={handleBack}
-          disabled={activeStep <= 0}
-        >
-          Back
-        </button>
-        <button
-          className="bg-primary text-white text-sm px-4 p-2 w-fit disabled:opacity-50"
-          onClick={handleNext}
-          disabled={activeStep >= 2}
-        >
-          {activeStep === steps.length - 1 ? "Confirm Payment" : "Next"}
-        </button>
+                      <div className="flex gap-2 justify-end w-full">
+                        <button
+                          className="bg-white text-primary px-4 p-2 w-fit disabled:text-text-variant"
+                          onClick={handleBack}
+                          disabled={activeStep <= 0}
+                        >
+                          {"BACK"}
+                        </button>
+                        <button
+                          className="bg-primary text-white  px-4 p-2 w-fit disabled:opacity-50"
+                          onClick={handleNext}
+                          disabled={activeStep >= 2}
+                        >
+                          {activeStep === steps.length - 1
+                            ? "Confirm Payment"
+                            : "CONTINUE"}
+                        </button>
+                      </div>
+                    </StepContent>
+                  </Step>
+                ))}
+              </Stepper>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
