6648f801110ab45aac4aae69df5cbaadd0bbf567
maurolps <boroengenharia@gmail.com>
2024-03-08 17:19:57 -0300

    add Payment Component

---
+++ src/Components/Checkout/CustomTextField.tsx
@@ -0,0 +1,29 @@
+import TextField from "@mui/material/TextField/TextField";
+import styled from "@mui/material/styles/styled";
+
+export const CustomTextField = styled((props) => (
+  <TextField InputProps={{ disableUnderline: true }} {...props} />
+))(({ theme }) => ({
+  "& .MuiFilledInput-root": {
+    overflow: "hidden",
+    borderRadius: 4,
+    backgroundColor: "rgba(var(--foreground-color))",
+    border: "1px solid",
+    borderColor: "white",
+    color: "rgba(var(--text-color), 0.7) ",
+    transition: theme.transitions.create([
+      "border-color",
+      "background-color",
+      "box-shadow",
+    ]),
+    "&:hover": {
+      backgroundColor: "transparent",
+      borderColor: "rgba(var(--primary-color), 0.3)",
+    },
+    "&.Mui-focused": {
+      backgroundColor: "transparent",
+      boxShadow: "rgba(var(--primary-color),0.5) 0 0 0 2px",
+      borderColor: "transparent",
+    },
+  },
+}));
---
+++ src/Components/Checkout/Payment.tsx
@@ -0,0 +1,68 @@
+import createTheme from "@mui/material/styles/createTheme";
+import { CustomTextField } from "./CustomTextField";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import InputAdornment from "@mui/material/InputAdornment/InputAdornment";
+import CreditCardIcon from "@mui/icons-material/CreditCard";
+import KeyIcon from "@mui/icons-material/Key";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: `rgb(var(--primary-color))`,
+    },
+    secondary: {
+      main: `rgb(var(--call-to-action-color))`,
+    },
+  },
+});
+
+export default function Payment() {
+  return (
+    <>
+      <ThemeProvider theme={theme}>
+        <div className="flex flex-col gap-2">
+          <CustomTextField
+            label="Card Number"
+            variant="filled"
+            defaultValue="5324 5678 9012 3456"
+            size="small"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                  <CreditCardIcon />
+                </InputAdornment>
+              ),
+              disableUnderline: true,
+            }}
+          />
+          <CustomTextField
+            label="Name on Card"
+            variant="filled"
+            defaultValue="John Doe"
+            size="small"
+          />
+          <div className="flex gap-2">
+            <CustomTextField
+              label="Expires"
+              variant="filled"
+              defaultValue="03/28"
+              size="small"
+              sx={{
+                width: "90px",
+              }}
+            />
+            <CustomTextField
+              label="CVC"
+              variant="filled"
+              defaultValue="527"
+              size="small"
+              sx={{
+                width: "90px",
+              }}
+            />
+          </div>
+        </div>
+      </ThemeProvider>
+    </>
+  );
+}
