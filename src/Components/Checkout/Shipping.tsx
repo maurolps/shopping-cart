d219c7ace86d7a32f3de54b510a6f4dfd0605e51
maurolps <boroengenharia@gmail.com>
2024-03-08 17:19:25 -0300

    mextract CustomTextField to module scope

--- src/Components/Checkout/Shipping.tsx
+++ src/Components/Checkout/Shipping.tsx
@@ -1,9 +1,7 @@
-import TextField, {
-  TextFieldVariants,
-} from "@mui/material/TextField/TextField";
+import { TextFieldVariants } from "@mui/material/TextField/TextField";
 import { ThemeProvider } from "@mui/material/styles";
 import createTheme from "@mui/material/styles/createTheme";
-import styled from "@mui/material/styles/styled";
+import { CustomTextField } from "./CustomTextField";
 
 const theme = createTheme({
   palette: {
@@ -15,33 +13,6 @@ const theme = createTheme({
     },
   },
 });
-
-const CustomTextField = styled((props) => (
-  <TextField InputProps={{ disableUnderline: true }} {...props} />
-))(({ theme }) => ({
-  "& .MuiFilledInput-root": {
-    overflow: "hidden",
-    borderRadius: 4,
-    backgroundColor: "rgba(var(--foreground-color))",
-    border: "1px solid",
-    borderColor: "white",
-    color: "rgba(var(--text-color), 0.7) ",
-    transition: theme.transitions.create([
-      "border-color",
-      "background-color",
-      "box-shadow",
-    ]),
-    "&:hover": {
-      backgroundColor: "transparent",
-      borderColor: "rgba(var(--primary-color), 0.3)",
-    },
-    "&.Mui-focused": {
-      backgroundColor: "transparent",
-      boxShadow: "rgba(var(--primary-color),0.5) 0 0 0 2px",
-      borderColor: "transparent",
-    },
-  },
-}));
 
 export default function Shipping() {
   const styles = {
