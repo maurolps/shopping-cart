1f4e91a2c769753371cc79a1b9fb7cab2868da03
maurolps <boroengenharia@gmail.com>
2024-03-15 22:06:44 -0300

    add Redux store

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,5 @@
 import AllShoes from "./Components/AllShoes";
 import Banner from "./Components/Banner/Banner";
-import Cart from "./Components/Cart/Cart";
-import Checkout from "./Components/Checkout/Checkout";
 import Discount from "./Components/Discount";
 import Footer from "./Components/Footer";
 import { Header } from "./Components/Header/Header";
@@ -15,7 +13,7 @@ export default function App() {
         <Header />
       </header>
       <main className="flex justify-center min-w-[900px] max-w-screen-lg mx-auto px-4 flex-col gap-10">
-        {/* <section>
+        <section>
           <Banner />
         </section>
         <section>
@@ -29,8 +27,7 @@ export default function App() {
         </section>
         <section>
           <Discount />
-        </section> */}
-        <Checkout />
+        </section>
       </main>
 
       <footer className="bg-primary w-full min-h-40 my-7 min-w-[900px] max-w-screen-lg mx-auto">
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import { StyledEngineProvider } from "@mui/material";
+import { Provider } from "react-redux";
+import { store } from "./store/store.ts";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    {/* <StyledEngineProvider injectFirst> */}
-    <App />
-    {/* </StyledEngineProvider> */}
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
