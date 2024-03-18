1290f9fb4d377cfdd3c8b2f4486556f2bced8e19
maurolps <boroengenharia@gmail.com>
2024-03-18 17:07:25 -0300

    add cartSlice

---
+++ src/features/cartSlice.ts
@@ -0,0 +1,28 @@
+import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+
+type Products = {
+  id: number;
+  name: string;
+  price: number;
+  type: string;
+  sale: number;
+  stars: number;
+};
+
+export const cartSlice = createSlice({
+  name: "cart",
+  initialState: {
+    items: [] as Products[],
+  },
+  reducers: {
+    addProduct: (state, action: PayloadAction<Products>) => {
+      state.items.push(action.payload);
+    },
+    removeProduct: (state, action) => {
+      state.items = state.items.filter((item) => item.id !== action.payload);
+    },
+  },
+});
+
+export const { addProduct, removeProduct } = cartSlice.actions;
+export default cartSlice.reducer;
--- src/store/store.ts
+++ src/store/store.ts
@@ -1,7 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
+import cartSlice from "../features/cartSlice";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    cart: cartSlice,
+  },
 });
 
 export type AppDispatch = typeof store.dispatch;
