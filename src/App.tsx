91f82bc130f87cd6888192e0dc3a97a848b07361
maurolps <boroengenharia@gmail.com>
2024-05-28 19:10:06 -0300

    add fetchImageUrls to localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -10,11 +10,37 @@ import Cart from "./Components/Cart/Cart";
 import { useAppSelector, useAppDispatch } from "./features/hooks";
 import { toggleCart } from "./features/cartSlice";
 import { Toaster } from "sonner";
+import { storage } from "./features/firebase";
+import { getDownloadURL, listAll, ref } from "firebase/storage";
 import { Product } from "./Components/Product";
+import { useEffect, useState } from "react";
+import { fetchImageUrls } from "./features/fetchImageUrls";
+
+type TImageUrl = {
+  name: string;
+  url: string;
+};
 
 export default function App() {
   const openCart = useAppSelector((store) => store.cart.toggleCart);
   const dispatch = useAppDispatch();
+  const [imageUrls, setImageUrls] = useState<TImageUrl[]>([]);
+
+  useEffect(() => {
+    const storedImageUrls = localStorage.getItem("scartImages");
+    if (storedImageUrls) {
+      setImageUrls(JSON.parse(storedImageUrls));
+      console.log("loaded from local storage:");
+      console.table(JSON.parse(storedImageUrls));
+    } else {
+      fetchImageUrls().then((res) => {
+        setImageUrls(res);
+        localStorage.setItem("scartImages", JSON.stringify(res));
+        console.log("loaded from firebase:");
+        console.table(res);
+      });
+    }
+  }, []);
 
   return (
     <>
@@ -43,6 +69,9 @@ export default function App() {
             </div>
           </Drawer>
         </section>
+        <section>
+          <Trending />
+        </section>
         {/* <section>
           <Banner />
         </section>
@@ -58,9 +87,6 @@ export default function App() {
         <section>
           <Discount />
         </section> */}
-        <section>
-          <Product />
-        </section>
       </main>
 
       <footer className="bg-primary w-full min-h-40 my-7 min-w-[900px] max-w-screen-lg mx-auto">
