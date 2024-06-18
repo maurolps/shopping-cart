16e57ce4f6d94bf4a2117a38f3b88fae467e4f40
maurolps <boroengenharia@gmail.com>
2024-06-18 19:50:08 -0300

    add special offer product

--- src/Components/SpecialOffer.tsx
+++ src/Components/SpecialOffer.tsx
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+import { useAppSelector } from "../features/hooks";
+import { specialOffer } from "./mockData.json";
+
+type TImageUrl = {
+  name: string;
+  url: string;
+};
+
 export function SpecialOffer() {
+  const storedImgUrls = useAppSelector((state) => state.products.imageUrls);
+  const [imgUrl, setImgUrl] = useState<string | undefined>("");
+
+  useEffect(() => {
+    const findImgUrl = (imgUrls: TImageUrl[], productName: string) => {
+      if (imgUrls !== undefined) {
+        return imgUrls.find((item) => item.name.includes(productName))?.url;
+      }
+    };
+
+    setImgUrl(findImgUrl(storedImgUrls, "Special Offer - 2.png"));
+  }, [storedImgUrls]);
+
   return (
     <>
       <div className="grid grid-cols-2 gap-20  p-10  min-w-[900px] max-w-screen-lg mx-auto bg-gradient-to-r from-primary via-slate-500 to-slate-500">
         <div className="p-10 flex flex-col">
           <div className="text-white text-sm uppercase">Special Offer</div>
           <div className="text-white text-3xl font-bold p-4 uppercase">
-            NEW <br /> Asics Gel-Kayano Ultrawave
+            NEW <br /> {specialOffer.name}
           </div>
           <div className="text-foreground text-sm  p-4">
             <p>
@@ -23,7 +45,17 @@ export function SpecialOffer() {
             </button>
           </div>
         </div>
-        <div className="w-full h-96 bg-slate-600"></div>
+        <div className="flex flex-col justify-center items-center border-2 border-primary bg-foreground">
+          {imgUrl && (
+            <img
+              loading="lazy"
+              src={imgUrl}
+              className="object-contain  w-[90%] h-[90%]"
+              alt=""
+              style={{ rotate: "-25deg", scale: "1.2" }}
+            />
+          )}
+        </div>
       </div>
     </>
   );
