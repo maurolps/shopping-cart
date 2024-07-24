3bd7ae0214a06381728c268a936f48513bb6eb1b
maurolps <boroengenharia@gmail.com>
2024-07-23 22:45:33 -0300

    move firebase integration to serverside

---
+++ backend/config/firebaseConfig.ts
@@ -0,0 +1,10 @@
+import "dotenv/config";
+
+export default {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: "shopping-cart-d8714.firebaseapp.com",
+  projectId: "shopping-cart-d8714",
+  storageBucket: "shopping-cart-d8714.appspot.com",
+  messagingSenderId: "202112691947",
+  appId: "1:202112691947:web:f80c2e37ff4c673389d8ae",
+};
--- backend/server.ts
+++ backend/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import shoes from "./mockShoes";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3001;
 const shoesRoute = "/shoes";
 
 app.use(express.json());
