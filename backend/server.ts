5be7f14c1a74803588ae6e1b7629fece5710d3f8
maurolps <boroengenharia@gmail.com>
2024-07-25 00:00:30 -0300

    temp script to populate database

--- backend/server.ts
+++ backend/server.ts
@@ -1,18 +1,53 @@
-import express from "express";
+// import express from "express";
+
+// const app = express();
+// const port = process.env.PORT || 3001;
+// const shoesRoute = "/shoes";
+
+// app.use(express.json());
+
+// app.get(shoesRoute, (req, res) => {
+//   res.status(200).json(shoes);
+// });
+
+// app.listen(port, () => {
+//   console.log(
+//     `GET request server started at http://localhost:${port}${shoesRoute}`
+//   );
+// });
+
 import shoes from "./mockShoes";
+import { PrismaClient } from "@prisma/client";
 
-const app = express();
-const port = process.env.PORT || 3001;
-const shoesRoute = "/shoes";
+const prisma = new PrismaClient();
 
-app.use(express.json());
+async function main() {
+  const categories = [
+    "training",
+    "running",
+    "walking",
+    "trending",
+    "specialOffer",
+  ];
 
-app.get(shoesRoute, (req, res) => {
-  res.status(200).json(shoes);
-});
-
-app.listen(port, () => {
-  console.log(
-    `GET request server started at http://localhost:${port}${shoesRoute}`
+  await Promise.all(
+    categories.map((category) => {
+      if (shoes[category]) {
+        return prisma[category].createMany({
+          data: shoes[category],
+        });
+      }
+    })
   );
-});
+
+  console.log("data inserted.");
+}
+
+main()
+  .catch(async (e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
