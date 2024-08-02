1c79f85c9211a0592db165bd257627ea7c4facde
maurolps <boroengenharia@gmail.com>
2024-08-02 17:25:50 -0300

    add cors

--- backend/app.ts
+++ backend/app.ts
@@ -1,9 +1,11 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
 import router from "./routes/shoes";
 
 app.use(router);
 app.use(express.json());
+app.use(cors());
 
 export default app;
