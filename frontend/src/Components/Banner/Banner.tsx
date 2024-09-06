6f1914faebaa81af769e759d130e57b958bf0d77
maurolps <boroengenharia@gmail.com>
2024-09-05 21:45:14 -0300

    update mobile ux

--- frontend/src/Components/Banner/Banner.tsx
+++ frontend/src/Components/Banner/Banner.tsx
@@ -1,5 +1,4 @@
 import { Link } from "react-router-dom";
-import { svgRotate } from "../../assets/svgs";
 import { Suspense, lazy } from "react";
 
 const Shoe3d = lazy(() => import("./Shoe3d"));
@@ -7,7 +6,7 @@ const Shoe3d = lazy(() => import("./Shoe3d"));
 // Prevent THREE warnings, adapted from mrdoob
 // discussion: https://github.com/mrdoob/three.js/pull/5791
 const _consoleLog = console.log;
-console.log = function (...args) {
+console.log = function(...args) {
   if (typeof args[0] === "string" && args[0].substring(0, 5) === "THREE") {
     return;
   }
@@ -17,32 +16,27 @@ console.log = function (...args) {
 export default function Banner() {
   return (
     <>
-      <div className="grid grid-cols-2 gap-20  p-10  relative min-w-[900px] max-w-screen-lg mx-auto bg-gradient-to-r from-primary via-slate-500 to-slate-500">
-        <div className=" flex flex-col gap-6 max-w-lg justify-self-end ">
-          <span className="font-revans text-7xl text-end z-10 text-white pointer-events-none select-none pr-6">
-            Fitness, <br /> Meets Comfort.
+      <div className="grid grid-cols-2 p-6 gap-2  relative max-w-screen-lg mx-auto bg-gradient-to-r from-primary via-slate-500 to-slate-500">
+        <div className="flex max-w-lg justify-self-end items-center ">
+          <span className="font-revans text-4xl sm:text-5xl lg:text-7xl text-end z-10 text-white pointer-events-none select-none ">
+            Fitness, <br /> Meets <br /> Comfort.
           </span>
         </div>
-        <div className="flex flex-col gap-6 relative justify-self-start">
-          <div className="z-10 flex justify-center items-end h-full pointer-events-none select-none min-h-64">
-            <span className="text-start text-sm max-w-sm text-text-variant ">
-              Our mission is to elevate your every step, offering an extensive
-              selection of footwear that merges fitness-forward trends with
-              unparalleled comfort and quality.{" "}
-              <Link to="marketplace">
-                <button className="bg-call text-white text-[10px] font-bold w-fit px-1 pointer-events-auto">
-                  TAKE A LOOK
-                </button>
-              </Link>
-            </span>
-          </div>
-          <div className="w-full h-full absolute aspect-square">
+        <div className="relative">
+          <div className="w-48 sm:w-64 min-[800px]:w-72 lg:w-80 aspect-[3/2] absolute top-5 left-1/2 right-1/2 transform -translate-x-1/2 ">
             <Suspense fallback={""}>
               <Shoe3d />
             </Suspense>
           </div>
-          <div className="pointer-events-none top-1 right-10 absolute aspect-square">
-            {svgRotate}
+          <div className="min-h-56 sm:min-h-60 lg:min-h-64 z-10 flex justify-center items-end pointer-events-none select-none">
+            <span className="text-start text-[10px] min-[375px]:text-xs sm:text-sm max-w-sm text-text-variant ">
+              Our mission is to elevate your every step, offering an extensive
+              selection of footwear that merges fitness-forward trends with
+              unparalleled comfort and quality. {" "}
+              <span className="bg-call text-white text-[9px] font-bold w-fit px-1 pointer-events-auto">
+                <Link to="marketplace"> TAKE A LOOK </Link>
+              </span>
+            </span>
           </div>
         </div>
       </div>
