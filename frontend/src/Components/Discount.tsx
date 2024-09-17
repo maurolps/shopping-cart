b2660b8f29b54a634254d67eb2055971acb5fdc3
maurolps <boroengenharia@gmail.com>
2024-09-17 19:47:25 -0300

    add redux methods

--- frontend/src/Components/Discount.tsx
+++ frontend/src/Components/Discount.tsx
@@ -1,38 +1,55 @@
 import TextField from "@mui/material/TextField/TextField";
-import { useState } from "react";
+import { applyDiscount } from "../features/cartSlice";
+import { useAppDispatch, useAppSelector } from "../features/hooks";
+import ThemeProvider from "@mui/material/styles/ThemeProvider";
+import createTheme from "@mui/material/styles/createTheme";
+
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: `rgb(var(--primary-color))`,
+    },
+  },
+});
 
 export default function Discount() {
-
-  const [status, setStatus] = useState(' ');
+  const dispatch = useAppDispatch();
+  const userName = useAppSelector((store) => store.cart.resume.userName);
+  let statusMessage = ' ';
+  if (userName !== 'johndoe') {
+    statusMessage = `Welcome ${userName}! Discount applied to checkout.`;
+  }
 
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const data = new FormData(e.currentTarget).get('email') as string;
     const name = data?.split("@")[0];
-    if (!name) { setStatus("Please enter a valid email address.") } else {
-      setStatus(`Welcome ${name}! Discount applied to checkout.`);
+    if (!name) { statusMessage = "Please enter a valid email address." } else {
+      dispatch(applyDiscount(name));
     }
-    console.log(name);
   };
 
   return (
     <div className="w-full m-auto flex items-center py-2 justify-center bg-foreground min-h-40 shadow-sm">
       <div className="flex flex-col gap-6">
         <span className="text-xl sm:text-2xl font-bold text-center">
-          Get Discount 30% Off
+          Get Discount 20% Off
         </span>
         <form onSubmit={handleSubmit} className="flex flex-col sm:flex-row items-center sm:items-start gap-1 sm:gap-4 justify-center max-w-full">
-          <TextField
-            name="email"
-            label="Email"
-            type="email"
-            variant="outlined"
-            size="small"
-            disabled={status !== ' '}
-            placeholder="Email"
-            helperText={status}
-          />
-          {status === ' ' &&
+          <ThemeProvider theme={theme}>
+            <TextField
+              name="email"
+              label={userName !== 'johndoe' ? 'Discount applied' : 'Email'}
+              type="email"
+              variant="outlined"
+              size="small"
+              disabled={userName !== 'johndoe'}
+              placeholder='Enter your email'
+              helperText={statusMessage}
+            />
+          </ThemeProvider>
+          {userName === 'johndoe' &&
             <button type='submit' className="bg-primary text-white text-sm px-4 p-2 w-fit uppercase h-[40px] rounded">
               Subscribe
             </button>
